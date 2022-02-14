dfaf8c8e66119f67ef81ccf61ede8a80e1bf123a
Aswath <54670655+aswath01@users.noreply.github.com>
2022-02-14 13:39:17 +0530

    Add files via upload

---
+++ simon-game/index.js
@@ -0,0 +1,6 @@
+// $(document).ready(function() {
+//     alert("hi");
+// })
+var arrayOfColors = ["red", "blue", "green", "yellow"];
+var rand = Math.ceil(Math.random() * 3);
+document.querySelector("button").st
